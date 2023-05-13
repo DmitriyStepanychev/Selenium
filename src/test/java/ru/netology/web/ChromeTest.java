5e3c86d967b283cd1f38da4a9003fcca3aeac07d
Dmitriy Stepanychev <mabcroshi@gmail.com>
2023-05-13 11:42:45 +0300

    Change Driver manager connect to setup

--- src/test/java/ru/netology/web/ChromeTest.java
+++ src/test/java/ru/netology/web/ChromeTest.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
@@ -23,6 +24,7 @@ class ChromeTest {
     @BeforeAll
     static void setupAll() {
 //        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
@@ -32,7 +34,6 @@ class ChromeTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver = WebDriverManager.chromedriver().create();
     }
 
     @AfterEach
