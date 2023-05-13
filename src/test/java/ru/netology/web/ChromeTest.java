f542e1dfe34a85107af170b55840a07f19307d22
Dmitriy Stepanychev <mabcroshi@gmail.com>
2023-05-13 11:26:26 +0300

    Change Driver manager

--- src/test/java/ru/netology/web/ChromeTest.java
+++ src/test/java/ru/netology/web/ChromeTest.java
@@ -6,12 +6,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 class ChromeTest {
 
@@ -19,7 +22,7 @@ class ChromeTest {
 
     @BeforeAll
     static void setupAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
     }
 
     @BeforeEach
@@ -29,6 +32,7 @@ class ChromeTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver = WebDriverManager.chromedriver().create();
     }
 
     @AfterEach
