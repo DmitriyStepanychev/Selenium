045487c55458acb57e01684c610d6a82599a63a9
Dmitriy Stepanychev <mabcroshi@gmail.com>
2023-05-15 06:38:06 +0300

    Updated negative tests

--- src/test/java/ru/netology/web/ChromeTest.java
+++ src/test/java/ru/netology/web/ChromeTest.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
@@ -72,6 +74,24 @@ class ChromeTest {
         Boolean expected = true;
         Boolean actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid")).isEnabled();
         assertEquals(expected, actual);
+        String expected2 = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual2 = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub")).getText().trim();
+        assertEquals(expected2, actual2);
     }
 
+    @Test
+    void testWithLonPhoneNumber() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+792133030301");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        Boolean expected = true;
+        Boolean actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid")).isEnabled();
+        assertEquals(expected, actual);
+        String expected2 = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual2 = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub")).getText().trim();
+        assertEquals(expected2, actual2);
+    }
 }
